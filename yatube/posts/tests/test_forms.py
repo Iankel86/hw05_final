231bbbcc4770bf6f9062db2d8897b6319a9bd6eb
Ян Шадрин <Yankelll86@mail.ru>
2022-04-21 12:08:35 +0300

    мой 10 комментарий

---
+++ yatube/posts/tests/test_forms.py
@@ -0,0 +1,148 @@
+from http import HTTPStatus
+
+from django.contrib.auth import get_user_model
+from django.test import Client, TestCase
+from django.urls import reverse
+
+from posts.models import Group, Post
+
+User = get_user_model()
+
+
+class PostFormTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.user = User.objects.create_user(username='test_user')
+        cls.group = Group.objects.create(
+            title='Тест группа',
+            slug='testgroup',
+            description='Тест описание',
+        )
+        cls.post = Post.objects.create(
+            text='Тестовый текст',
+            author=cls.user,
+            group=cls.group,
+        )
+        cls.form_data = {
+            'text': cls.post.text,
+            'group': cls.group.id,
+        }
+
+    def setUp(self):
+        self.authorized_client = Client()
+        self.authorized_client.force_login(PostFormTest.user)
+
+    def test_create_post(self):
+        post_count = Post.objects.count()
+        context = {
+            'text': 'Текстовый текст',
+            'group': PostFormTest.group.id,
+        }
+        response = self.authorized_client.post(
+            reverse('posts:post_create'),
+            data=context,
+            follow=True
+        )
+        self.assertRedirects(response,
+                             reverse('posts:profile',
+                                     kwargs={
+                                         'username': PostFormTest.user}))
+        self.assertEqual(Post.objects.count(), post_count + 1)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(Post.objects.latest('id').text, context['text'])
+        self.assertEqual(Post.objects.latest('id').group, PostFormTest.group)
+
+    def test_post_edit(self):
+        form_data = {
+            'text': 'Отредактированный текст поста',
+            'group': self.group.id
+        }
+        response = self.authorized_client.post(
+            reverse('posts:post_edit', args=[self.post.pk]),
+            data=form_data,
+            follow=True
+        )
+        self.assertRedirects(
+            response,
+            reverse('posts:post_detail', args=[self.post.id])
+        )
+        self.assertTrue(
+            Post.objects.filter(
+                text=form_data['text'],
+                group=self.group.id,
+                id=self.post.id,
+                author=PostFormTest.user,
+            ).exists()
+        )
+
+    def test_anonim_client_create_post(self):
+        post_count = Post.objects.count()
+        response = self.client.post(
+            reverse('posts:post_create'),
+            data=PostFormTest.form_data,
+            follow=True
+        )
+        self.assertEqual(Post.objects.count(), post_count)
+        self.assertRedirects(response,
+                             reverse('users:login') + '?next=' + reverse(
+                                 'posts:post_create'))
+        self.assertEqual(Post.objects.count(), post_count)
+
+    def test_anonim_edit_post(self):
+        context = {
+            'text': 'Попытка изменить пост',
+            'group': self.group.id,
+        }
+        response = self.client.post(
+            reverse('posts:post_edit', kwargs={
+                'post_id': PostFormTest.post.id}),
+            data=context,
+            follow=True
+        )
+        self.assertRedirects(response, reverse(
+            'users:login') + '?next=' + reverse(
+            'posts:post_edit', kwargs={'post_id': PostFormTest.post.id}))
+        edited_post = Post.objects.get(id=PostFormTest.post.id)
+        self.assertNotEqual(edited_post.text, context['text'])
+        self.assertNotEqual(edited_post.group, context['group'])
+
+    def test_create_post_without_group(self):
+        post_count = Post.objects.count()
+        context = {
+            'text': 'Текстовый текст',
+        }
+        response = self.authorized_client.post(
+            reverse('posts:post_create'),
+            data=context,
+            follow=True
+        )
+        self.assertRedirects(response,
+                             reverse('posts:profile',
+                                     kwargs={
+                                         'username': PostFormTest.user}))
+        self.assertEqual(Post.objects.count(), post_count + 1)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(Post.objects.latest('id').text, context['text'])
+
+    def test_edit_post_not_author(self):
+        user_editor = User.objects.create(
+            username='editor_not_owner_post'
+        )
+        authorized_editor = Client()
+        authorized_editor.force_login(user_editor)
+
+        form_data = {
+            'text': 'Отредактированный текст поста',
+        }
+        response = authorized_editor.post(
+            reverse('posts:post_edit', args=[self.post.pk]),
+            data=form_data,
+            follow=True
+        )
+        post = Post.objects.get(id=self.post.pk)
+        self.assertEqual(post.text, self.post.text)
+        self.assertRedirects(
+            response,
+            reverse('posts:post_detail', args=[self.post.id])
+        )
---
+++ yatube/posts/views.py
@@ -0,0 +1,125 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect, render
+
+from .forms import CommentForm, PostForm
+from .models import Comment, Group, Post, User
+from .utils import get_paginator
+
+
+def index(request):
+    posts = Post.objects.all()
+    page_obj, total_count = get_paginator(Post.objects.all(), request)
+    context = {
+        'posts': posts,
+        'page_obj': page_obj,
+        'total_count': total_count,
+    }
+    return render(request, 'posts/index.html', context)
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    page_obj, total_count = get_paginator(group.posts.all(), request)
+    context = {
+        'group': group,
+        'total_count': total_count,
+        'page_obj': page_obj
+    }
+    return render(request, 'posts/group_list.html', context)
+
+
+def profile(request, username):
+    author = get_object_or_404(User, username=username)
+    page_obj, posts_amount = get_paginator(author.posts.all(), request)
+    context = {
+        # Все посты пользователя в profile.html
+        'author': author,
+        # Всего постов автора в profile.html
+        'posts_amount': posts_amount,
+        # Список всех постов отсорт по 10 шт и привяз. к панинатору
+        'page_obj': page_obj,
+    }
+    return render(request, 'posts/profile.html', context)
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    author_posts = post.author.posts.count()
+    num_author_posts = Post.objects.filter(author=author_posts).count()
+    comment_form = CommentForm(request.POST or None)
+    comments = Comment.objects.filter(post=post)
+    context = {
+        'post': post,
+        'author_posts': author_posts,
+        'posts_count': num_author_posts,
+        'form': comment_form,
+        'comments': comments,
+    }
+    return render(request, 'posts/post_detail.html', context)
+
+
+@login_required
+def post_create(request):
+    tamplate = 'posts/create_post.html'
+    form = PostForm(request.POST or None, files=request.FILES or None)
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.author = request.user
+        post.save()
+        return redirect('posts:profile', username=request.user.username)
+    return render(request, tamplate, {'form': form})
+
+
+@login_required
+def post_edit(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    template = 'posts/create_post.html'
+    if post.author != request.user:
+        return redirect('posts:post_detail', post_id=post.pk)
+    form = PostForm(request.POST or None,
+                    files=request.FILES or None,
+                    instance=post)
+    if form.is_valid():
+        form.save()
+        return redirect('posts:post_detail', post_id=post.pk)
+    context = {
+        'form': form,
+        'post': post,
+        'is_edit': True,
+    }
+    return render(request, template, context)
+
+
+@login_required
+def add_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    form = CommentForm(request.POST or None)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.author = request.user
+        comment.post = post
+        comment.save()
+    return redirect('posts:post_detail', post_id=post_id)
+
+# def test_delete_image(self):
+#         """Проверяем, что картинку можно удалить из существующего поста"""
+#         post_count = Post.objects.count()
+#         form_data = {
+#             'text': self.post.text,
+#             'group': self.post.group.id,
+#             'image-clear': 'on'
+#         }
+#         response = self.author_client.post(
+#             reverse('posts:post_edit', kwargs={'post_id': self.post.id}),
+#             data=form_data
+#         )
+
+#         self.assertRedirects(
+#             response, reverse(
+#                 'posts:post_detail', kwargs={'post_id': self.post.id}
+#             )
+#         )
+#         self.assertEqual(Post.objects.count(), post_count,
+#                          f'Создан новый пост')
+#         self.post.refresh_from_db()
+#         self.assertEqual(self.post.image.name, '')
